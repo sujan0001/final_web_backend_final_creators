d234b336ee8e136c60d655a89ca2d5f670fc9237
sujan bhujel <sujanbhujel1333@gmail.com>
2025-07-12 12:24:50 +0545

    up to date with backend, testing left

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(
+            "mongodb://localhost:27017/sujan_backend",
+            {
+                useNewUrlParser:true,
+                useUnifiedTopology:true
+            }
+        )
+    }catch(err){
+        console.log("DB Err", err)
+    }
+};
+module.exports= connectDB;
--- index.js
+++ index.js
@@ -2,14 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
-const userRoutes=require('./routes/userRoute')
-const categoryRouteAdmin= require('./routes/admin/categoryRouteAdmin')
-const productRouteAdmin = require("./routes/admin/productRouteAdmin");
-const userRoutesAdmin = require('./routes/admin/userRouteAdmin');
-const ribbonRouteAdmin=require('./routes/admin/ribbonRouteAdmin')
+const userRoute=require('./routes/userRoute')
+const collectionRoutes = require("./routes/collectionRoutes");
+const productRoute=require('./routes/productRoute')
 const app = express();
 const path = require("path") 
 const PORT = process.env.PORT || 5050;
+
 
 
 
@@ -18,20 +17,25 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-
-app.use("/api/auth",userRoutes)
-app.use("/api/admin/category", categoryRouteAdmin)
-app.use("/api/admin/ribbon", ribbonRouteAdmin)
-app.use("/api/admin/product", productRouteAdmin);
-app.use("/api/admin/user", userRoutesAdmin);
-app.use("/api/admin/user", require("./routes/admin/userRouteAdmin"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 console.log("Serving static files from:", path.join(__dirname, "uploads"));
 
+//routes
+app.use("/api/auth",userRoute);
+app.use("/api/collections", collectionRoutes);
+app.use("/api/product", productRoute);
+
+
+
+
+
+
+//test route
 app.get('/hey', (req, res) => {
   res.send('Hello World!');
 });
 
+//start server
 app.listen(PORT, () => {
   console.log(`Server running on port number ${PORT}`);
 });
